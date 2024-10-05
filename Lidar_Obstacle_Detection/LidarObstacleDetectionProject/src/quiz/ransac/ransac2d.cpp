671441ee37fef1734546e12c023087cc9de72c85
Raghad AlMalki <raghadeidalmalki@gmail.com>
2024-10-05 21:21:03 +0300

    Update ransac2d.cpp

--- Lidar_Obstacle_Detection/LidarObstacleDetectionProject/src/quiz/ransac/ransac2d.cpp
+++ Lidar_Obstacle_Detection/LidarObstacleDetectionProject/src/quiz/ransac/ransac2d.cpp
@@ -1,4 +1,3 @@
-/* \author Aaron Brown */
 // Quiz on implementing simple RANSAC line fitting
 
 #include "../../render/render.h"
@@ -66,57 +65,6 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 	std::unordered_set<int> inliersResult;
 	srand(time(NULL));
 	
-	// TODO: Fill in this function
-
-	// For max iterations 
-
-	// Randomly sample subset and fit line
-
-	// Measure distance between every point and fitted line
-	// If distance is smaller than threshold count it as inlier
-
-	// Return indicies of inliers from fitted line with most inliers
-  	//my code
-//  	while(maxIterations)
-//    {
-    //randomly pic two points
-//    std::unordered_set<int> inliers;
-//	while (inliers.size() < 2) 
-//      	inliers.insert(rand()%(cloud->points.size()));
-
-//	float x1, y1, x2, y2;
-
-//	auto itr = inliers.begin();
-//	x1 = cloud->points[*itr].x;
-//	y1 = cloud->points[*itr].y;
-//	itr++;
-//	x2 = cloud->points[*itr].x;
-//	y2 = cloud->points[*itr].y;
-
-//	float a = (y1-y2);
-//	float b = (x2-x1);
-//	float c = (x1*y2-x2*y1);
-
-//	for(int index =0; index < cloud->points.size(); index++)
-//    {
-//	    if(inliers.count(index)>0)
-//			continue;
-
-//		pcl::PointXYZ point = cloud->points[index];
-//		float x3 = point.x;
-//		float y3 = point.y;
-
-//		float d = fabs(a*x3+b*y3+c)/sqrt(a*a+b*b);
-
-//		if(d <= distanceTol)
-//			inliers.insert(index); 
-//    }
-// 	 if(inliers.size()>inliersResult.size())
-// 	{
-//		inliersResult = inliers;
-//  	}   
-//    }
-	//end of my code
   
     while (maxIterations--) {
         std::unordered_set<int> inliers;
